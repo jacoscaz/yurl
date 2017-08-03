7f4c0d8a0fcea463f31f27eaa7a81dbf54ba32d6
Jacopo Scazzosi <me@jacoscaz.com>
2017-08-03 11:54:46 +0200

    es6

--- index.js
+++ index.js
@@ -1,163 +1,131 @@
 
-var url = require('url');
-var path = require('path');
-var assert = require('assert');
+'use strict';
 
-function extend(target) {
-  var sources = Array.prototype.slice.call(arguments);
-  var source, s, keys, key, k;
-  for (s = 0; s < sources.length; s++) {
-    source = sources[s];
-    keys = Object.keys(source);
-    for (k = 0; k < keys.length; k++) {
-      key = keys[k];
-      if (source[key] !== undefined) {
-        target[key] = source[key];
-      }
+const _ = require('lodash');
+const url = require('url');
+const path = require('path');
+const assert = require('assert');
+
+class _YURL {
+
+  constructor(urlString, parse, slashes) {
+    assert(_.isString(urlString), 'urlString is not an object.');
+    this._parse = parse !== false;
+    this._slashes = slashes !== false;
+    this._parts = url.parse(urlString, this._parse, this._slashes);
+  }
+
+  set(parts) {
+    assert(_.isObject(parts), '`parts` is not an object.');
+    _.extend(this._parts, parts);
+    return this;
+  }
+
+  format() {
+    return url.format(this._parts);
+  }
+
+  clone() {
+    return new _YURL(this.format(), this._parse, this._slashes);
+  }
+
+  host(host) {
+    if (host === false) {
+      return this.set({host: null, hostname: null, port: null, href: null});
     }
+    assert(_.isString(host), '`host is not a string.');
+    return this.set({host: host, hostname: null, port: null, href: null});
   }
-  return target;
-}
 
-function deepCopy(source) {
-  var copy = source;
-  var keys, key, k;
-  if (source && typeof source === 'object') {
-    if (Array.isArray(source)) {
-      copy = [];
-      for (k = 0; k < source.length; k++) {
-        copy[k] = deepCopy(source[k]);
-      }
-    } else {
-      copy = {};
-      keys = Object.keys(source);
-      for (k = 0; k < keys.length; k++) {
-        key = keys[k];
-        copy[key] = deepCopy(source[key]);
-      }
+  hostname(hostname) {
+    if (hostname === false) {
+      return this.set({hostname: null, host: null, href: null});
     }
+    assert(_.isString(hostname), '`hostname` is not a string.');
+    return this.set({hostname: hostname, host: null, href: null});
   }
-  return copy;
-}
 
-function YURL(urlString, parse, slashes) {
-  if (!(this instanceof YURL)) {
-    return new YURL(urlString, parse, slashes);
+  protocol(protocol) {
+    if (protocol === false) {
+      return this.set({protocol: null, href: null});
+    }
+    assert(_.isString(protocol), '`protocol` is not a string.');
+    return this.set({protocol: protocol, href: null});
   }
-  assert(typeof(urlString) === 'string', 'urlString is not an object.');
-  this._parse = parse !== false;
-  this._slashes = slashes !== false;
-  this._parts = url.parse(urlString, this._parse, this._slashes);
-}
 
-module.exports = YURL;
-
-YURL.prototype.set = function(parts) {
-  assert(typeof(parts) === 'object', '`parts` is not an object.');
-  extend(this._parts, parts);
-  return this;
-}
-
-YURL.prototype.format = function() {
-  return url.format(this._parts);  
-}
-
-YURL.prototype.clone = function() {
-  return new YURL(this.format(), this._parse, this._slashes);
-}
-
-YURL.prototype.host = function(host) {
-  if (host === false) {
-    return this.set({host: null, hostname: null, port: null, href: null});
+  slashes(slashes) {
+    assert(typeof(slashes) === 'boolean', '`slashes` is not a boolean value.');
+    return this.set({slashes: slashes, href: null});
   }
-  assert(typeof(host) === 'string', '`host is not a string.');
-  return this.set({host: host, hostname: null, port: null, href: null});
-}
 
-YURL.prototype.hostname = function(hostname) {
-  if (hostname === false) {
-    return this.set({hostname: null, host: null, href: null});
+  port(port) {
+    if (port === false) {
+      return this.set({port: null, host: null, href: null});
+    }
+    assert(typeof(port) === 'number', '`port` is not a number.');
+    return this.set({port: port, host: null, href: null});
   }
-  assert(typeof(hostname) === 'string', '`hostname` is not a string.');
-  return this.set({hostname: hostname, host: null, href: null});
-}
 
-YURL.prototype.protocol = function(protocol) {
-  if (protocol === false) {
-    return this.set({protocol: null, href: null});
+  pathname(pathname) {
+    if (pathname === false) {
+      return this.set({pathname: null, path: null, href: null});
+    }
+    const args = Array.prototype.slice.call(arguments);
+    args.unshift(this._parts.pathname || '/');
+    return this.set({pathname: path.posix.resolve.apply(path.posix, args), path: null, href: null});
   }
-  assert(typeof(protocol) === 'string', '`protocol` is not a string.');
-  return this.set({protocol: protocol, href: null});
-}
 
-YURL.prototype.slashes = function(slashes) {
-  assert(typeof(slashes) === 'boolean', '`slashes` is not a boolean value.');
-  return this.set({slashes: slashes, href: null});
-}
-
-YURL.prototype.port = function(port) {
-  if (port === false) {
-    return this.set({port: null, host: null, href: null});
+  path(path) {
+    if (path === false) {
+      return this.set({pathname: null, path: null, href: null});
+    }
+    assert(_.isString(path), '`path` is not a string.');
+    const parts = url.parse(path, true);
+    return this.set({
+      path: parts.path,
+      href: parts.href,
+      search: parts.search,
+      query: parts.query,
+      hash: parts.hash,
+      pathname: parts.pathname
+    });
   }
-  assert(typeof(port) === 'number', '`port` is not a number.');
-  return this.set({port: port, host: null, href: null});
-}
 
-YURL.prototype.pathname = function(pathname) {
-  if (pathname === false) {
-    return this.set({pathname: null, path: null, href: null});
+  search(search) {
+    if (search === false) {
+      return this.set({search: null, query: null, href: null});
+    }
+    assert(_.isString(search), '`search` is not a string.');
+    search.charAt(0) !== '?' && (search = '?' + search);
+    return this.set({search: search});
   }
-  var args = Array.prototype.slice.call(arguments);
-  args.unshift(this._parts.pathname || '/');
-  return this.set({pathname: path.posix.resolve.apply(path.posix, args), path: null, href: null});
-}
 
-YURL.prototype.path = function(path) {
-  if (path === false) {
-    return this.set({pathname: null, path: null, href: null});
+  hash(hash) {
+    if (hash === false) {
+      return this.set({hash: null, href: null});
+    }
+    assert(_.isString(hash), '`hash` is not a string.');
+    hash.charAt(0) !== '#' && (hash = '#' + hash);
+    return this.set({hash: hash, href: null});
   }
-  assert(typeof(path) === 'string', '`path` is not a string.');
-  var parts = url.parse(path, true);
-  return this.set({
-    path: parts.path, 
-    href: parts.href, 
-    search: parts.search, 
-    query: parts.query, 
-    hash: parts.hash, 
-    pathname: parts.pathname
-  });
-}
 
-YURL.prototype.search = function(search) {
-  if (search === false) {
-    return this.set({search: null, query: null, href: null});
+  auth(auth) {
+    if (auth === false) {
+      return this.set({auth: null, href: null});
+    }
+    assert(_.isString(auth), '`auth` is not a string.');
+    return this.set({auth: auth, href: null});
   }
-  assert(typeof(search) === 'string', '`search` is not a string.');
-  search.charAt(0) != '?' && (search = '?' + search);
-  return this.set({search: search});
-}
 
-YURL.prototype.hash = function(hash) {
-  if (hash === false) {
-    return this.set({hash: null, href: null});
+  query(args) {
+    if (args === false) {
+      return this.set({query: {}, href: null, search: null})
+    }
+    return this.set({query: _.extend(this._parts.query || {}, args), href: null, search: null});
   }
-  assert(typeof(hash) === 'string', '`hash` is not a string.');
-  hash.charAt(0) != '#' && (hash = '#' + hash);
-  return this.set({hash: hash, href: null});
+
 }
 
-YURL.prototype.auth = function(auth) {
-  if (auth === false) {
-    return this.set({auth: null, href: null});
-  }
-  assert(typeof(auth) === 'string', '`auth` is not a string.');
-  return this.set({auth: auth, href: null});
-}
-
-YURL.prototype.query = function(args) {
-  if (args === false) {
-    return this.set({query: {}, href: null, search: null})
-  }
-  return this.set({query: extend(this._parts.query || {}, args), href: null, search: null});
-}
-
+module.exports = function YURL(urlString, parse, slashes) {
+  return new _YURL(urlString, parse, slashes);
+};
